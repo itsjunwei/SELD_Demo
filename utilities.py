9c5efa72563b8ccbb8cbdfc79722482c1ffd0d2a
Yeow Jun Wei <junwei268@gmail.com>
2025-01-31 10:00:36 +0800

    Updated composite cutout aug

--- utilities.py
+++ utilities.py
@@ -463,119 +463,158 @@ class DataAugmentNumpyBase:
         raise NotImplementedError
 
 
-class CompositeCutout(DataAugmentNumpyBase):
+
+class CompositeCutout:
     """
-    This data augmentation combine Random cutout, specaugment, cutout hole.
+    This data augmentation combines Random Cutout, SpecAugment, and Random Cutout Hole.
     """
     def __init__(self, always_apply: bool = False, p: float = 0.5, image_aspect_ratio: float = 1,
                  n_zero_channels: int = None, is_filled_last_channels: bool = True):
         """
-        :param n_zero_channels: if given, these last n_zero_channels will be filled in zeros instead of random values
-        :param is_filled_last_channels: if False, does not cutout n_zero_channels
+        :param always_apply: If True, always apply transformations regardless of probability.
+        :param p: Probability to apply transformations if always_apply is False.
+        :param image_aspect_ratio: Aspect ratio for Random Cutout.
+        :param n_zero_channels: If given, these last n_zero_channels will be filled with zeros instead of random values.
+        :param is_filled_last_channels: If False, does not fill n_zero_channels with zeros.
         """
-        super().__init__(always_apply, p)
-        self.random_cutout = RandomCutoutNp(always_apply=True, image_aspect_ratio=image_aspect_ratio,
-                                            n_zero_channels=n_zero_channels,
-                                            is_filled_last_channels=is_filled_last_channels)
-        self.spec_augment = SpecAugmentNp(always_apply=True, n_zero_channels=n_zero_channels,
-                                          is_filled_last_channels=is_filled_last_channels)
-        self.random_cutout_hole = RandomCutoutHoleNp(always_apply=True, n_zero_channels=n_zero_channels,
-                                                     is_filled_last_channels=is_filled_last_channels)
+        self.always_apply = always_apply
+        self.p = p
+        self.random_cutout = RandomCutoutTensor(
+            always_apply=True,
+            image_aspect_ratio=image_aspect_ratio,
+            n_zero_channels=n_zero_channels,
+            is_filled_last_channels=is_filled_last_channels
+        )
+        self.spec_augment = SpecAugmentTensor(
+            always_apply=True,
+            n_zero_channels=n_zero_channels,
+            is_filled_last_channels=is_filled_last_channels
+        )
+        self.random_cutout_hole = RandomCutoutHoleTensor(
+            always_apply=True,
+            n_zero_channels=n_zero_channels,
+            is_filled_last_channels=is_filled_last_channels
+        )
 
-    def apply(self, x: np.ndarray):
-        choice = np.random.randint(0, 3, 1)[0]
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Apply one of the augmentation techniques randomly.
+        
+        :param x: Input tensor of shape (n_channels, n_time_steps, n_features) or (n_time_steps, n_features).
+        :return: Augmented tensor.
+        """
+        if not self.always_apply and torch.rand(1).item() > self.p:
+            return x
+
+        choice = torch.randint(0, 3, (1,)).item()
         if choice == 0:
             return self.random_cutout(x)
         elif choice == 1:
             return self.spec_augment(x)
         elif choice == 2:
             return self.random_cutout_hole(x)
+        else:
+            return x  # Fallback in case of unexpected choice
 
 
-class RandomCutoutNp(DataAugmentNumpyBase):
+
+
+class RandomCutoutTensor:
     """
-    This data augmentation randomly cutout a rectangular area from the input image. Tested.
+    This data augmentation randomly cuts out a rectangular area from the input tensor.
     """
     def __init__(self, always_apply: bool = False, p: float = 0.5, image_aspect_ratio: float = 1,
                  random_value: float = None, n_zero_channels: int = None, is_filled_last_channels: bool = True):
         """
         :param always_apply: If True, always apply transform.
-        :param p: If always_apply is false, p is the probability to apply transform.
-        :param image_aspect_ratio: height/width ratio. For spectrogram: n_time_steps/ n_features.
-        :param random_value: random value to fill in the cutout area. If None, randomly fill the cutout area with value
-            between min and max of input.
-        :param n_zero_channels: if given, these last n_zero_channels will be filled in zeros instead of random values
-        :param is_filled_last_channels: if False, does not cutout n_zero_channels
+        :param p: If always_apply is False, p is the probability to apply transform.
+        :param image_aspect_ratio: Height/width ratio. For spectrogram: n_time_steps / n_features.
+        :param random_value: Random value to fill in the cutout area. If None, randomly fill the cutout area with value
+                             between min and max of input.
+        :param n_zero_channels: If given, these last n_zero_channels will be filled with zeros instead of random values.
+        :param is_filled_last_channels: If False, does not cutout n_zero_channels.
         """
-        super().__init__(always_apply, p)
+        self.always_apply = always_apply
+        self.p = p
         self.random_value = random_value
         self.n_zero_channels = n_zero_channels
         self.is_filled_last_channels = is_filled_last_channels
-        # Params: s: area, r: height/width ratio.
+
+        # Parameters for area and aspect ratio
         self.s_l = 0.02
         self.s_h = 0.3
         self.r_1 = 0.3
         self.r_2 = 1 / 0.3
         if image_aspect_ratio > 1:
-            self.r_1 = self.r_1 * image_aspect_ratio
+            self.r_1 *= image_aspect_ratio
         elif image_aspect_ratio < 1:
-            self.r_2 = self.r_2 * image_aspect_ratio
+            self.r_2 *= image_aspect_ratio
 
-    def apply(self, x: np.ndarray) -> np.ndarray:
-        """
-        :param x: <(n_channels, n_time_steps, n_features) or (n_time_steps, n_features)>: input spectrogram.
-        :return: random cutout x
-        """
-        # get image size
-        image_dim = x.ndim
-        img_h = x.shape[-2]  # time frame dimension
-        img_w = x.shape[-1]  # feature dimension
-        min_value = np.min(x)
-        max_value = np.max(x)
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        if not self.always_apply and torch.rand(1).item() > self.p:
+            return x
+
+        # Get image size
+        image_dim = x.dim()
+        img_h, img_w = x.shape[-2], x.shape[-1]
+
+        min_value = torch.min(x)
+        max_value = torch.max(x)
+
         # Initialize output
-        output_img = x.copy()
-        # random erase
-        s = np.random.uniform(self.s_l, self.s_h) * img_h * img_w
-        r = np.random.uniform(self.r_1, self.r_2)
-        w = np.min((int(np.sqrt(s / r)), img_w - 1))
-        h = np.min((int(np.sqrt(s * r)), img_h - 1))
-        left = np.random.randint(0, img_w - w)
-        top = np.random.randint(0, img_h - h)
+        output_img = x.clone()
+
+        # Random erase
+        s = torch.empty(1).uniform_(self.s_l, self.s_h).item() * img_h * img_w
+        r = torch.empty(1).uniform_(self.r_1, self.r_2).item()
+        w = min(int(torch.sqrt(torch.tensor(s / r)).item()), img_w - 1)
+        h = min(int(torch.sqrt(torch.tensor(s * r)).item()), img_h - 1)
+
+        if img_w - w <= 0 or img_h - h <= 0:
+            # If the calculated width or height is too large, skip cutout
+            return output_img
+
+        left = torch.randint(0, img_w - w, (1,)).item()
+        top = torch.randint(0, img_h - h, (1,)).item()
+
         if self.random_value is None:
-            c = np.random.uniform(min_value, max_value)
+            c = torch.empty(1).uniform_(min_value, max_value).item()
         else:
             c = self.random_value
+
         if image_dim == 2:
             output_img[top:top + h, left:left + w] = c
         else:
             if self.n_zero_channels is None:
                 output_img[:, top:top + h, left:left + w] = c
             else:
-                output_img[:-self.n_zero_channels,  top:top + h, left:left + w] = c
+                output_img[:-self.n_zero_channels, top:top + h, left:left + w] = c
                 if self.is_filled_last_channels:
                     output_img[-self.n_zero_channels:, top:top + h, left:left + w] = 0.0
 
         return output_img
 
 
-class SpecAugmentNp(DataAugmentNumpyBase):
+
+class SpecAugmentTensor:
     """
-    This data augmentation randomly remove horizontal or vertical strips from image. Tested
+    This data augmentation randomly removes horizontal or vertical strips from the input tensor.
     """
     def __init__(self, always_apply: bool = False, p: float = 0.5, time_max_width: int = None,
                  freq_max_width: int = None, n_time_stripes: int = 1, n_freq_stripes: int = 1,
                  n_zero_channels: int = None, is_filled_last_channels: bool = True):
         """
         :param always_apply: If True, always apply transform.
-        :param p: If always_apply is false, p is the probability to apply transform.
-        :param time_max_width: maximum time width to remove.
-        :param freq_max_width: maximum freq width to remove.
-        :param n_time_stripes: number of time stripes to remove.
-        :param n_freq_stripes: number of freq stripes to remove.
-        :param n_zero_channels: if given, these last n_zero_channels will be filled in zeros instead of random values
-        :param is_filled_last_channels: if False, does not cutout n_zero_channels
+        :param p: If always_apply is False, p is the probability to apply transform.
+        :param time_max_width: Maximum time width to remove.
+        :param freq_max_width: Maximum frequency width to remove.
+        :param n_time_stripes: Number of time stripes to remove.
+        :param n_freq_stripes: Number of frequency stripes to remove.
+        :param n_zero_channels: If given, these last n_zero_channels will be filled with zeros instead of random values.
+        :param is_filled_last_channels: If False, does not cutout n_zero_channels.
         """
-        super().__init__(always_apply, p)
+        self.always_apply = always_apply
+        self.p = p
         self.time_max_width = time_max_width
         self.freq_max_width = freq_max_width
         self.n_time_stripes = n_time_stripes
@@ -583,33 +622,34 @@ class SpecAugmentNp(DataAugmentNumpyBase):
         self.n_zero_channels = n_zero_channels
         self.is_filled_last_channels = is_filled_last_channels
 
-    def apply(self, x: np.ndarray) -> np.ndarray:
-        """
-        :param x: <(n_channels, n_time_steps, n_features)>: input spectrogram.
-        :return: augmented spectrogram.
-        """
-        assert x.ndim == 3, 'Error: dimension of input spectrogram is not 3!'
-        n_frames = x.shape[1]
-        n_freqs = x.shape[2]
-        min_value = np.min(x)
-        max_value = np.max(x)
-        if self.time_max_width is None:
-            time_max_width = int(0.15 * n_frames)
-        else:
-            time_max_width = self.time_max_width
-        time_max_width = np.max((1, time_max_width))
-        if self.freq_max_width is None:
-            freq_max_width = int(0.2 * n_freqs)
-        else:
-            freq_max_width = self.freq_max_width
-        freq_max_width = np.max((1, freq_max_width))
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        if not self.always_apply and torch.rand(1).item() > self.p:
+            return x
 
-        new_spec = x.copy()
+        # Ensure input has 3 dimensions
+        assert x.dim() == 3, 'Error: dimension of input spectrogram is not 3!'
 
-        for i in np.arange(self.n_time_stripes):
-            dur = np.random.randint(1, time_max_width, 1)[0]
-            start_idx = np.random.randint(0, n_frames - dur, 1)[0]
-            random_value = np.random.uniform(min_value, max_value, 1)
+        n_channels, n_frames, n_freqs = x.shape
+        min_value = torch.min(x)
+        max_value = torch.max(x)
+
+        # Determine time and frequency widths
+        time_max_width = int(0.15 * n_frames) if self.time_max_width is None else self.time_max_width
+        time_max_width = max(1, time_max_width)
+
+        freq_max_width = int(0.2 * n_freqs) if self.freq_max_width is None else self.freq_max_width
+        freq_max_width = max(1, freq_max_width)
+
+        new_spec = x.clone()
+
+        # Apply time stripes
+        for _ in range(self.n_time_stripes):
+            dur = torch.randint(1, time_max_width + 1, (1,)).item()
+            if n_frames - dur <= 0:
+                continue  # Skip if duration is too large
+            start_idx = torch.randint(0, n_frames - dur + 1, (1,)).item()
+            random_value = torch.empty(1).uniform_(min_value, max_value).item()
+
             if self.n_zero_channels is None:
                 new_spec[:, start_idx:start_idx + dur, :] = random_value
             else:
@@ -617,10 +657,14 @@ class SpecAugmentNp(DataAugmentNumpyBase):
                 if self.is_filled_last_channels:
                     new_spec[-self.n_zero_channels:, start_idx:start_idx + dur, :] = 0.0
 
-        for i in np.arange(self.n_freq_stripes):
-            dur = np.random.randint(1, freq_max_width, 1)[0]
-            start_idx = np.random.randint(0, n_freqs - dur, 1)[0]
-            random_value = np.random.uniform(min_value, max_value, 1)
+        # Apply frequency stripes
+        for _ in range(self.n_freq_stripes):
+            dur = torch.randint(1, freq_max_width + 1, (1,)).item()
+            if n_freqs - dur <= 0:
+                continue  # Skip if duration is too large
+            start_idx = torch.randint(0, n_freqs - dur + 1, (1,)).item()
+            random_value = torch.empty(1).uniform_(min_value, max_value).item()
+
             if self.n_zero_channels is None:
                 new_spec[:, :, start_idx:start_idx + dur] = random_value
             else:
@@ -630,56 +674,65 @@ class SpecAugmentNp(DataAugmentNumpyBase):
 
         return new_spec
 
-class RandomCutoutHoleNp(DataAugmentNumpyBase):
+
+
+class RandomCutoutHoleTensor:
     """
-    This data augmentation randomly cutout a few small holes in the spectrogram. Tested.
+    This data augmentation randomly cuts out a few small holes in the input tensor.
     """
     def __init__(self, always_apply: bool = False, p: float = 0.5, n_max_holes: int = 8, max_h_size: int = 8,
                  max_w_size: int = 8, filled_value: float = None, n_zero_channels: int = None,
                  is_filled_last_channels: bool = True):
         """
         :param always_apply: If True, always apply transform.
-        :param p: If always_apply is false, p is the probability to apply transform.
+        :param p: If always_apply is False, p is the probability to apply transform.
         :param n_max_holes: Maximum number of holes to cutout.
         :param max_h_size: Maximum time frames of the cutout holes.
-        :param max_w_size: Maximum freq bands of the cutout holes.
-        :param filled_value: random value to fill in the cutout area. If None, randomly fill the cutout area with value
-            between min and max of input.
-        :param n_zero_channels: if given, these last n_zero_channels will be filled in zeros instead of random values
-        :param is_filled_last_channels: if False, does not cutout n_zero_channels
+        :param max_w_size: Maximum frequency bands of the cutout holes.
+        :param filled_value: Random value to fill in the cutout area. If None, randomly fill the cutout area with value
+                             between min and max of input.
+        :param n_zero_channels: If given, these last n_zero_channels will be filled with zeros instead of random values.
+        :param is_filled_last_channels: If False, does not cutout n_zero_channels.
         """
-        super().__init__(always_apply, p)
+        self.always_apply = always_apply
+        self.p = p
         self.n_max_holes = n_max_holes
-        self.max_h_size = np.max((max_h_size, 5))
-        self.max_w_size = np.max((max_w_size, 5))
+        self.max_h_size = max(max_h_size, 5)
+        self.max_w_size = max(max_w_size, 5)
         self.filled_value = filled_value
         self.n_zero_channels = n_zero_channels
         self.is_filled_last_channels = is_filled_last_channels
 
-    def apply(self, x: np.ndarray):
-        """
-        :param x: <(n_channels, n_time_steps, n_features)>: input spectrogram.
-        :return: augmented spectrogram.
-        """
-        assert x.ndim == 3, 'Error: dimension of input spectrogram is not 3!'
-        img_h = x.shape[-2]  # time frame dimension
-        img_w = x.shape[-1]  # feature dimension
-        min_value = np.min(x)
-        max_value = np.max(x)
-        new_spec = x.copy()
-        # n_cutout_holes = np.random.randint(1, self.n_max_holes, 1)[0]
-        n_cutout_holes = self.n_max_holes
-        for ihole in np.arange(n_cutout_holes):
-            # w = np.random.randint(4, self.max_w_size, 1)[0]
-            # h = np.random.randint(4, self.max_h_size, 1)[0]
-            w = self.max_w_size
-            h = self.max_h_size
-            left = np.random.randint(0, img_w - w)
-            top = np.random.randint(0, img_h - h)
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        if not self.always_apply and torch.rand(1).item() > self.p:
+            return x
+
+        # Ensure input has 3 dimensions
+        assert x.dim() == 3, 'Error: dimension of input spectrogram is not 3!'
+
+        n_channels, img_h, img_w = x.shape
+        min_value = torch.min(x)
+        max_value = torch.max(x)
+
+        new_spec = x.clone()
+
+        n_cutout_holes = self.n_max_holes  # You can randomize this if desired
+
+        for _ in range(n_cutout_holes):
+            w = self.max_w_size  # Alternatively, use torch.randint(4, self.max_w_size + 1, (1,)).item()
+            h = self.max_h_size  # Alternatively, use torch.randint(4, self.max_h_size + 1, (1,)).item()
+
+            if img_w - w <= 0 or img_h - h <= 0:
+                continue  # Skip if hole size is too large
+
+            left = torch.randint(0, img_w - w + 1, (1,)).item()
+            top = torch.randint(0, img_h - h + 1, (1,)).item()
+
             if self.filled_value is None:
-                filled_value = np.random.uniform(min_value, max_value)
+                filled_value = torch.empty(1).uniform_(min_value, max_value).item()
             else:
                 filled_value = self.filled_value
+
             if self.n_zero_channels is None:
                 new_spec[:, top:top + h, left:left + w] = filled_value
             else:
@@ -688,6 +741,7 @@ class RandomCutoutHoleNp(DataAugmentNumpyBase):
                     new_spec[-self.n_zero_channels:, top:top + h, left:left + w] = 0.0
 
         return new_spec
+
 
 class RandomShiftUpDownNp(DataAugmentNumpyBase):
     """
